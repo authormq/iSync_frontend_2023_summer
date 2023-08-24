717cecf999c8538206e43750af4fd8261d109145
Pan-Pan-Pan <1425650574@qq.com>
2023-08-24 13:27:48 +0800

    修改文件结构

---
+++ src/views/team/TeamView.vue
@@ -0,0 +1,24 @@
+<!-- 登录后的展示页 -->
+<!-- zby -->
+<template>
+  <div class="outer-container">
+    <ul>
+      <li>团队信息</li>
+      <li>团队成员</li>
+      <li>团队项目</li>
+      <li>团队群聊</li>
+    </ul>
+    <div>mq 接个路由</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeamView',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+</style>

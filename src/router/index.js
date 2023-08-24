6a16f7a2e95658c0dcb86f365eab107655821bd2
authormq <109514806+authormq@users.noreply.github.com>
2023-08-24 16:16:27 +0800

    project和team路由跳转 看下面的描述
    这里有一点bug 有一行有一点问题，在文件中用注释进行了说明 在查找解决方法

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-// import UserView from '../views/UserView.vue'
-// import TeamView from '../views/team/TeamView.vue'
-// import ChatListView from  '../views/team/subroute/ChatListView.vue'
-// import InfoView from  '../views/team/subroute/InfoView.vue'
-// import MemberListView from  '../views/team/subroute/MemberListView.vue'
-// import ProjectListView from  '../views/team/subroute/ProjectListView.vue'
-// import ProjectView from '../views/project/ProjectView.vue'
-// import DocListView from '../views/project/subroute/DocListView.vue'
-// import PageListView from '../views/project/subroute/PageListView.vue'
-// import MessageView from '../views/MessageView.vue'
+import UserView from '../views/UserView.vue'
+import TeamView from '../views/team/TeamView.vue'
+import ChatListView from  '../views/team/subroute/ChatListView.vue'
+import InfoView from  '../views/team/subroute/InfoView.vue'
+import MemberListView from  '../views/team/subroute/MemberListView.vue'
+import ProjectListView from  '../views/team/subroute/ProjectListView.vue'
+import ProjectView from '../views/project/ProjectView.vue'
+import DocListView from '../views/project/subroute/DocListView.vue'
+import PageListView from '../views/project/subroute/PageListView.vue'
+import MessageView from '../views/MessageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,54 +18,54 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    // },
-    // {
-    //   path: '/user',
-    //   component: UserView
-    // },
-    // {
-    //   path: 'message',
-    //   component: MessageView
-    // },
-    // {
-    //   path: '/team/all',
-    //   component: TeamView
-    // },
-    // {
-    //   path: `/team/${teamid}`,
-    //   component: TeamView,
-    //   children: [
-    //     {
-    //       path: 'info',
-    //       component: InfoView
-    //     },
-    //     {
-    //       path: 'chat',
-    //       component: ChatListView
-    //     },
-    //     {
-    //       path: 'member',
-    //       component: MemberListView
-    //     },
-    //     {
-    //       path: 'project',
-    //       component: ProjectListView
-    //     }
-    //   ]
-    // },
-    // {
-    //   path: `project/${projectid}`,
-    //   component: ProjectView,
-    //   children: [
-    //     {
-    //       path: 'doc',
-    //       component: DocListView
-    //     },
-    //     {
-    //       path: 'page',
-    //       component: PageListView
-    //     }
-    //   ]
+    },
+    {
+      path: '/user',
+      component: UserView
+    },
+    {
+      path: '/message',
+      component: MessageView
+    },
+    {
+      path: '/team/all',
+      component: TeamView
+    },
+    {
+      path: `/team/1`,
+      component: TeamView,
+      children: [
+        {
+          path: 'info',
+          component: InfoView
+        },
+        {
+          path: 'chat',
+          component: ChatListView
+        },
+        {
+          path: 'member',
+          component: MemberListView
+        },
+        {
+          path: 'project',
+          component: ProjectListView
+        }
+      ]
+    },
+    {
+      path: `/project/1`,
+      component: ProjectView,
+      children: [
+        {
+          path: 'doc',
+          component: DocListView
+        },
+        {
+          path: 'page',
+          component: PageListView
+        }
+      ]
     }
   ]
 })

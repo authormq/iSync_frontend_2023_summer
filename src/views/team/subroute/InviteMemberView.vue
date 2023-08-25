3f340e181decc9e61cccb24a8416a34b97f2f6f4
Iris-Q <109052116+Iris-Q@users.noreply.github.com>
2023-08-25 20:01:14 +0800

    修改团队信息对接口

--- src/views/team/subroute/InviteMemberView.vue
+++ src/views/team/subroute/InviteMemberView.vue
@@ -1,37 +1,39 @@
 <template>
-    <div>
-        <input type="text">
-        <button>这是搜索按钮</button>
-    </div>
-    <ul>
-        <li v-for="result in searchResult" :key="result">
-            {{ result }}
-            <button>这是添加按钮</button>
-        </li>
-    </ul>
-    <div>
-        邀请链接：{{ inviteLink }}
-    </div>
+  <div>
+    <input type="text">
+    <button>这是搜索按钮</button>
+  </div>
+  <ul>
+    <li v-for="result in searchResult" :key="result">
+      {{ result }}
+      <button>这是添加按钮</button>
+    </li>
+  </ul>
+  <div>
+    邀请链接：{{ inviteLink }}
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'InviteMemberView',
+  name: 'InviteMemberView',
     data() {
-        return {
-            searchResult: [{
-                'userId': '这是用户id',
-                'avatar': '这是用户头像',
-                'nickname': '这是昵称',
-            }, {
-                'userId': '这是用户id',
-                'avatar': '这是用户头像',
-                'nickname': '这是昵称',
-            }],
-            inviteLink: '这是邀请链接'
-        }
+      return {
+        searchResult: [{
+          'userId': '这是用户id',
+          'avatar': '这是用户头像',
+          'nickname': '这是昵称',
+        }, 
+        {
+          'userId': '这是用户id',
+          'avatar': '这是用户头像',
+          'nickname': '这是昵称',
+        }],
+          inviteLink: '这是邀请链接'
+      }
     }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>

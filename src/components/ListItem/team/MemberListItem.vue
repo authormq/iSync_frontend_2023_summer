c51a2f7e339c0d28772cb1086689b4cbf1123aec
authormq <109514806+authormq@users.noreply.github.com>
2023-08-26 03:04:34 +0800

    member一堆接口

--- src/components/ListItem/team/MemberListItem.vue
+++ src/components/ListItem/team/MemberListItem.vue
@@ -4,18 +4,26 @@
     <div class="member-info">
       <div class="member-info-basic">
         <div class="member-info-username">
-          {{ data.username }}
+          {{ data.lastName }}{{ data.firstName }}
         </div>
         <div class="member-info-realname">
-          {{ data.lastname }}{{ data.firstname }}
+          {{ data.username }}{{ data.email }}
         </div>
-        <div class="member-info-identity">
-          {{ data.identity }}
+        <div class="member-info-identity" @click="handleIdentityClick(data)">
+          {{ data.Identity }}
         </div>
       </div>
       <div class="member-info-profile">
         {{ data.profile }}
       </div>
+    </div>
+    <!-- 对信息进行更详细的展示，并且添加几个按钮，来展示需要进行身份变更 -->
+    <div>
+      <button v-if="(this.curIdentity === 'leader' || this.curIdentity === 'admin') && data.identity === 'member'" @click="setAdminOpt">设置为管理员</button>
+      <button v-if="this.curIdentity === 'leader' && data.identity === 'admin'"
+        @click="cancelAdminOpt">取消管理员身份</button>
+      <button v-if="this.curIdentity !== 'member' && data.identity === 'member'"
+        @click="deleteMemberOpt">移出团队</button>
     </div>
   </div>
 </template>
@@ -23,7 +31,36 @@
 <script>
 export default {
   name: 'MemberListItem',
-  props: ['data']
+  props: ['data', 'curIdentity'],
+  data() {
+    return {
+      handleIdentity: {
+        setAdmin: false,
+        cancelAdmin: false,
+        deleteMember: false
+      }
+    }
+  },
+  methods: {
+    handleIdentityClick() {
+      if (this.curIdentity === 'member') {
+        alert('你无权对其他成员的身份进行修改')
+      } else if (this.curIdentity === 'leader') {
+        this.$bus.emit('leaderHandleIdentity', { data: this.data, handleIdentity: this.handleIdentity })
+      } else {
+        this.$bus.emit('adminHandleIdentity', { data: this.data, handleIdentity: this.handleIdentity })
+      }
+    },
+    setAdminOpt() {
+      this.handleIdentity.setAdmin = true
+    },
+    cancelAdminOpt() {
+      this.handleIdentity.cancelAdmin = true
+    },
+    deleteMemberOpt() {
+      this.handleIdentity.deleteMember = true
+    }
+  }
 }
 </script>
 
@@ -40,6 +77,7 @@ export default {
   transition: 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
   padding-left: 5px;
 }
+
 .member-container:hover {
   box-shadow: 1px 1px 5px grey;
   transform: translate(-2px, -2px) scale(1.02);
@@ -87,7 +125,7 @@ img {
   background-color: grey;
   color: white;
   font-size: 14px;
-  border-radius: 5px; 
+  border-radius: 5px;
 }
 
 .member-info-profile {

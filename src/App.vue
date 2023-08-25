85842c68c0e4d29563600b217c23c5725de4c6d5
Iris-Q <109052116+Iris-Q@users.noreply.github.com>
2023-08-25 21:37:52 +0800

    增加选择团队的组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <TopNav v-if="'/'!==this.$route.path" />
-    <TeamSelector /> 
+    
   </div>
   <RouterView />
 </template>
 <script setup>
 import TopNav from './components/TopNav/TopNav.vue'
-import TeamSelector from './components/TopNav/TeamSelector.vue'
+
 </script>
 <style scoped>
 </style>

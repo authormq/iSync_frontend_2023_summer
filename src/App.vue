edef10c57d0aae8be81444431c62f653360e619d
Pan-Pan-Pan <1425650574@qq.com>
2023-08-26 12:34:11 +0800

    消息和邀请链接

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,35 @@
     <TopNav v-if="'/'!==this.$route.path" /> 
   </div>
   <RouterView />
+  <!-- 消息组件 -->
+  <StylishMessage :show="showMessage" :title="messageTitle" :content="messageContent"/>
   <!-- <text-editor /> -->
 </template>
+
 <script setup>
+import { ref, getCurrentInstance } from 'vue'
+
 import TopNav from './components/TopNav/TopNav.vue'
+import StylishMessage from './components/Stylish/StylishMessage.vue'
+
+let showMessage = ref(false)  // 展示消息
+let messageTitle = ref('')    // 消息标题
+let messageContent = ref('')  // 消息内容
+
+
+const instance = getCurrentInstance() // 获取 'vm'
+// 绑定全局事件
+instance.proxy.$bus.on('message', (data) => {
+  messageTitle.value = data.title
+  messageContent.value = data.content
+  showMessage.value = true
+  setTimeout(() => {
+    showMessage.value = false
+    messageTitle.value = ''
+    messageContent.value = ''
+  }, data.time)
+})
+
 </script>
 <style scoped>
 </style>

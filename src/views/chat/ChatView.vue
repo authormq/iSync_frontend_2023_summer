9c929938d8fe88629bc47570337d40fcbe3dc4e9
KaQyn <3140573900@qq.com>
2023-08-29 18:08:33 +0800

    4

--- src/views/chat/ChatView.vue
+++ src/views/chat/ChatView.vue
@@ -5,9 +5,12 @@
 		<vue-advanced-chat ref="test" theme="light" :styles="JSON.stringify(style)" height="calc(100vh - 110px)"
 			:current-user-id="currentUserId" :rooms="JSON.stringify(rooms)" :rooms-loaded="roomsLoaded"
 			:messages="JSON.stringify(messages)" :messages-loaded="messagesLoaded" :custom-search-room-enabled="true"
-			:show-add-room="false"
 			@send-message="sendMessage($event.detail[0])" @fetch-messages="fetchMessages($event.detail[0])"
-			@search-room="searchRoom($event.detail[0])" @open-file="openFile($event.detail[0])" />
+			@search-room="searchRoom($event.detail[0])" @open-file="openFile($event.detail[0])">
+			<!-- <div v-for="(message, index) in messages" :slot="'message_' + message._id" :key="index">
+ 				<div>hahaha{{ message.content }}</div>
+			</div> -->
+		</vue-advanced-chat>
 	</div>
 </template>
 
@@ -21,10 +24,10 @@ export default {
 	mounted() {
 		this.currentUserId = this.$cookies.get('user_id')
 		this.roomsLoaded = false
-		this.$http.get('/api/groups/list_by_team_id/', { params: { team_id: this.$route.params.teamId } }).then((response) => {
+		this.$http.get(`/api/groups/list_by_team_id/?team_id=${this.$route.params.teamId}`).then((response) => {
 			// rooms
-			this.rooms = response.data.map((group) => ({
-				index: this.rooms.length,
+			this.rooms = response.data.map((group, index) => ({
+				index: -index,
 				roomId: group.id,
 				roomName: group.is_private ? (
 					group.members[0].user.id == this.currentUserId ? group.members[1].user.username : group.members[0].user.username
@@ -32,11 +35,15 @@ export default {
 				avatar: group.is_private ? (
 					group.members[0].user.id == this.currentUserId ? group.members[1].user.avatar : group.members[0].user.avatar
 				) : group.avatar,
+				unreadCount: 0,
 				users: group.members.map((member) => ({
 					_id: `${member.user.id}`,
 					username: member.user.username
 				}))
 			}))
+			if (this.rooms.length > 0) {
+				this.currentRoomId = this.rooms[0].roomId
+			}
 			this.allRooms = this.rooms
 			// @all && last_message
 			for (let i = 0; i < this.rooms.length; i++) {
@@ -54,7 +61,7 @@ export default {
 					}
 				})
 				this.$http.get(`/api/groups/${this.rooms[i].roomId}/last_message/`).then((response) => {
-					if (response.status == 200) {
+					if (response.data != "") {
 						const message = response.data
 						this.rooms[i].lastMessage = {
 							_id: message.id,
@@ -62,15 +69,16 @@ export default {
 							senderId: `${message.sender.user.id}`,
 							username: message.sender.user.username,
 							avatar: message.sender.user.avatar,
-							timestamp: message.create_datetime.substring(11, 16).replace('-', ':'),
+							timestamp: message.create_datetime.substring(11, 16),
 							date: message.create_datetime.substring(5, 10),
 							new: false,
 						}
 					}
 				})
 				// WebSocket
-				this.ws[i] = new WebSocket(`ws://43.138.14.231:9000/ws/chat/group/${this.rooms[i].roomId}/${this.currentUserId}/`)
-				this.ws[i].onmessage = (messageEvent) => {
+				const index = this.rooms.length + this.rooms[i].index - 1
+				this.ws[index] = new WebSocket(`ws://43.138.14.231:9000/ws/chat/group/${this.rooms[i].roomId}/${this.currentUserId}/`)
+				this.ws[index].onmessage = (messageEvent) => {
 					const data = JSON.parse(messageEvent.data)
 					const message = data.message
 					this.rooms[i].lastMessage = {
@@ -79,7 +87,7 @@ export default {
 						senderId: `${message.sender.user.id}`,
 						username: message.sender.user.username,
 						avatar: message.sender.user.avatar,
-						timestamp: message.create_datetime.substring(11, 16).replace('-', ':'),
+						timestamp: message.create_datetime.substring(11, 16),
 						date: message.create_datetime.substring(5, 10),
 						new: true,
 						replyMessage: message.reply_message == null ? null : {
@@ -88,21 +96,29 @@ export default {
 							senderId: `${message.reply_message.sender.user.id}`,
 							username: message.reply_message.sender.user.username,
 							avatar: message.reply_message.sender.user.avatar,
-							timestamp: message.reply_message.create_datetime.substring(11, 16).replace('-', ':'),
+							timestamp: message.reply_message.create_datetime.substring(11, 16),
 							date: message.reply_message.create_datetime.substring(5, 10),
 							new: false,
 						},
 						files: message.file_content == null ? null : [{
-							name: message.file_content.name.split('message_file_')[0],
+							name: message.file_content.name.split('message/file/')[1],
 							size: message.file_content.size,
 							url: message.file_content.url,
 							type: message.file_content.name.split('.')[1]
 						}]
 					}
-					this.messages = [
-						...this.messages,
-						this.rooms[i].lastMessage
-					]
+					if (this.currentRoomId != this.rooms[i].roomId) {
+						this.rooms[i].unreadCount++
+					}
+					else {
+						this.messages = [
+							...this.messages,
+							this.rooms[i].lastMessage
+						]
+					}
+					const tmpRooms = this.rooms.filter(room => room.roomId != this.rooms[i].roomId)
+					this.rooms = [this.rooms[i], ...tmpRooms]
+					this.allRooms = this.rooms
 					// @
 					for (let j = 0; j < data.mentioned_users.length; j++) {
 						if (data.mentioned_users[j]._id == this.currentUserId || data.mentioned_users[j]._id == '0') {
@@ -135,16 +151,32 @@ export default {
 		return {
 			ws: [],
 			currentUserId: '',
+			currentRoomId: '',
 			rooms: [],
 			allRooms: [],
 			roomsLoaded: false,
 			messages: [],
-			messagesLoaded: false
+			messagesLoaded: false,
+			style: {
+				room: {
+					backgroundCounterBadge: 'rgba(199, 29, 35, 1)',
+				},
+				icons: {
+					add: 'rgba(199, 29, 35, 1)',
+					toggle: 'rgba(199, 29, 35, 1)',
+					dropdownScroll: 'rgba(199, 29, 35, 1)',
+					microphone: 'rgba(199, 29, 35, 1)',
+					emoji: 'rgba(199, 29, 35, 1)',
+					paperclip: 'rgba(199, 29, 35, 1)',
+					send: 'rgba(199, 29, 35, 1)',
+				}
+			}
 		}
 	},
 	methods: {
 		fetchMessages({ room, options }) {
 			this.messagesLoaded = false
+			this.currentRoomId = room.roomId
 			setTimeout(() => {
 				this.messages = []
 				this.$http.get(`/api/groups/${room.roomId}/messages/`).then((response) => {
@@ -154,7 +186,7 @@ export default {
 						senderId: `${message.sender.user.id}`,
 						username: message.sender.user.username,
 						avatar: message.sender.user.avatar,
-						timestamp: message.create_datetime.substring(11, 16).replace('-', ':'),
+						timestamp: message.create_datetime.substring(11, 16),
 						date: message.create_datetime.substring(5, 10),
 						new: false,
 						replyMessage: message.reply_message == null ? null : {
@@ -163,30 +195,31 @@ export default {
 							senderId: `${message.reply_message.sender.user.id}`,
 							username: message.reply_message.sender.user.username,
 							avatar: message.reply_message.sender.user.avatar,
-							timestamp: message.reply_message.create_datetime.substring(11, 16).replace('-', ':'),
+							timestamp: message.reply_message.create_datetime.substring(11, 16),
 							date: message.reply_message.create_datetime.substring(5, 10),
 							new: false,
 						},
 						files: message.file_content == null ? null : [{
-							name: message.file_content.name.split('message_file_')[0],
+							name: message.file_content.name.split('message/file/')[1],
 							size: message.file_content.size,
 							url: message.file_content.url,
 							type: message.file_content.name.split('.')[1]
 						}]
 					}))
 					this.messagesLoaded = true
+					room.unreadCount = 0
 				})
 			})
 		},
 
 		sendMessage(message) {
-			this.scrollToMessage(40)
 			for (let i = 0; i < this.rooms.length; i++) {
 				if (this.rooms[i].roomId == message.roomId) {
+					const index = this.rooms.length + this.rooms[i].index - 1
 					if (message.files != null) {
 						const reader = new FileReader()
 						reader.onload = (event) => {
-							this.ws[i].send(JSON.stringify({
+							this.ws[index].send(JSON.stringify({
 								'text_content': message.content,
 								'mentioned_users': message.usersTag,
 								'reply_message': message.replyMessage,
@@ -197,7 +230,7 @@ export default {
 						reader.readAsDataURL(message.files[0].blob)
 					}
 					else {
-						this.ws[i].send(JSON.stringify({
+						this.ws[index].send(JSON.stringify({
 							'text_content': message.content,
 							'mentioned_users': message.usersTag,
 							'reply_message': message.replyMessage,

2a2d723fc76fc8c320eb449ebb536d9e586369fc
KaQyn <3140573900@qq.com>
2023-08-30 14:47:56 +0800

    chat

--- src/views/chat/ChatView.vue
+++ src/views/chat/ChatView.vue
@@ -337,47 +337,56 @@ export default {
 				}
 			}
 		},
-		fetchMessages({ room, options }) {
-			this.messagesLoaded = false
+		fetchMessages({ room, options = {} }) {
 			this.currentRoomId = room.roomId
-			setTimeout(() => {
-				this.messages = []
-				this.$http.get(`/api/groups/${room.roomId}/messages/`).then((response) => {
-					this.messages = response.data.map((message) => ({
-						_id: message.id,
-						content: message.text_content,
-						senderId: `${message.sender.user.id}`,
-						username: message.sender.user.username,
-						avatar: message.sender.user.avatar,
-						timestamp: message.create_datetime.substring(11, 16),
-						date: message.create_datetime.substring(5, 10),
-						new: false,
-						replyMessage: message.reply_message == null ? null : {
-							_id: message.reply_message.id,
-							content: message.reply_message.text_content,
-							senderId: `${message.reply_message.sender.user.id}`,
-							username: message.reply_message.sender.user.username,
-							avatar: message.reply_message.sender.user.avatar,
-							timestamp: message.reply_message.create_datetime.substring(11, 16),
-							date: message.reply_message.create_datetime.substring(5, 10),
-							new: false,
-						},
-						files: message.file_content == null ? null : [{
-							name: message.file_content.name.split('message/file/')[1],
-							size: message.file_content.size,
-							url: message.file_content.url,
-							type: message.file_content.name.split('.')[1]
-						}]
-					}))
+			const offset = options.reset ? 0 : this.messages.length
+			if (options.reset) {
+				this.messagesLoaded = false
+			}
+			this.$http.get(`/api/groups/${room.roomId}/messages/?limit=30&offset=${offset}`).then((response) => {
+				if (response.data.results.length == 0) {
 					this.messagesLoaded = true
-					for (let i = 0; i < this.rooms.length; i++) {
-						if (this.rooms[i].roomId == room.roomId) {
-							this.rooms[i].unreadCount = 0
-							break
-						}
-					}
-				})
+					return
+				}
+				const messages = response.data.results.map((message) => ({
+					_id: message.id,
+					content: message.text_content,
+					senderId: `${message.sender.user.id}`,
+					username: message.sender.user.username,
+					avatar: message.sender.user.avatar,
+					timestamp: message.create_datetime.substring(11, 16),
+					date: message.create_datetime.substring(5, 10),
+					new: false,
+					replyMessage: message.reply_message == null ? null : {
+						_id: message.reply_message.id,
+						content: message.reply_message.text_content,
+						senderId: `${message.reply_message.sender.user.id}`,
+						username: message.reply_message.sender.user.username,
+						avatar: message.reply_message.sender.user.avatar,
+						timestamp: message.reply_message.create_datetime.substring(11, 16),
+						date: message.reply_message.create_datetime.substring(5, 10),
+						new: false,
+					},
+					files: message.file_content == null ? null : [{
+						name: message.file_content.name.split('message/file/')[1],
+						size: message.file_content.size,
+						url: message.file_content.url,
+						type: message.file_content.name.split('.')[1]
+					}]
+				})).reverse()
+				if (options.reset) {
+					this.messages = messages
+				}
+				else {
+					this.messages = [...messages, ...this.messages]
+				}
 			})
+			for (let i = 0; i < this.rooms.length; i++) {
+				if (this.rooms[i].roomId == room.roomId) {
+					this.rooms[i].unreadCount = 0
+					break
+				}
+			}
 		},
 
 		sendMessage(message) {

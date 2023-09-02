4504d3b164a794357296fbd6e47b4657071e5d5f
authormq <109514806+authormq@users.noreply.github.com>
2023-09-02 16:15:04 +0800

    邀请新成员进群

---
+++ src/components/Modal/InviteNewMemberModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <StylishModal :show="false" width="500px" height="700px" padding="25px" shadow="3px 3px 10px grey">
+    <div class="container-header">
+      <div class="header-title">
+        邀请成员进群
+      </div>
+      <div @click="handleClose">
+        <CloseIcon size="30"></CloseIcon>
+      </div>
+    </div>
+  </StylishModal>
+</template>
+<script>
+import StylishModal from '../Stylish/StylishModal.vue';
+import CloseIcon from '../Svg/CloseIcon.vue';
+export default {
+  name: 'InviteNewMemberModal',
+  props: ['show'],
+  emits: ['close'],
+  components: { StylishModal, CloseIcon },
+  methods: {
+    handleClose() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style scoped>
+.container-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  margin-bottom: 5px;
+}
+
+.header-title {
+  font-size: 30px;
+  font-weight: bold;
+  color: rgba(199, 29, 35, 1);
+  padding-left: 130px;
+
+}
+</style>
